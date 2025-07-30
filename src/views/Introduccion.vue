12fb0ecf5311e6d995e9f6c7b22ce90cb817ed17
cveimarsena <vcelis@sena.edu.co>
2025-07-30 15:34:52 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -22,7 +22,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="1246" height="701" src="https://www.youtube.com/embed/ULkgujykK-o" title="Gestión de novedades en la afiliación al SGSSS con enfoque diferencial y atención al usuario" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
 
 </template>
 
